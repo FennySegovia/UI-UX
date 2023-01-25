4a2a40856bea1b711b802c2f3cec96dbfe64c6ec
FennySegovia <118381877+FennySegovia@users.noreply.github.com>
2023-01-26 00:05:51 +0800

    Add files via upload

---
+++ UIUX/UIUX.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="main.css" rel="stylesheet"/>
+
+</head>
+
+<body>
+
+<header id="header" class="fixed-top ">
+    <div class="container-fluid">
+
+      <div class="row justify-content-center">
+        <div class="col-xl-9 d-flex align-items-center justify-content-lg-between">
+          <h1 class="logo me-auto me-lg-0">PROTONTECH</a></h1>
+
+          <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+              <li><a class="nav-link scrollto active" href="#hero">HOME</a></li>
+              <li><a class="nav-link scrollto" href="#services">SERVICES</a></li>
+              <li><a class="nav-link scrollto " href="#products">PRODUCTS</a></li>
+              <li class="dropdown"><a href="#"><span>MAKE A REQUEST</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a class="nav-link scrollto" href="#">REPAIR</a></li>
+              <li><a class="nav-link scrollto" href="#">REQUEST</a></li>
+            </ul>
+          </li>
+
+              <li><a class="nav-link scrollto" href="#about">ABOUT</a></li>
+              <li><a class="nav-link scrollto" href="#contact">CONTACT</a></li>
+
+            </ul>
+          </nav>
+
+          <a class="btn btn-outline-success mr-2" role="submit" href="#">Request Status</a>
+        </div>
+      </div>
+
+    </div>
+  </header>
+
+<div class="container">
+<section id="hero" class="d-flex flex-column justify-content-center">
+    <div class="container">
+      <div class="row justify-content-center text-center mb-4">
+        <div class="col-xl-8">
+                <h2>Start Exploring now.</h2>
+            </div>
+            <p class="text-center mb-4">
+                We offer a wide range of repair for most types of electronics and
+                Job orders like CCTV installation. Simply fill out our online form
+                to request a repair or Job order and one of our technicians will
+                be in touch with you soon.
+            </p>
+            <div class="text-center">
+                <a class="btn btn-outline-success mr-2" role="submit" href="#">Request a Repair</a>
+                <a class="btn btn-outline-success" role="submit" href="#">Request a custom service</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+                <!-- Value 1 -->
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100 text-center logoss hidden">
+                        <div class="card-body">
+                            <i class="fas fa-thumbs-up fa-4x mb-4 value-icon-2"></i>
+                            <h5 class="card-title">Customer Satisfaction</h5>
+                            <p class="card-text">
+                                We are committed to delivering the best possible experience
+                                to our clients, and strive to exceed their expectations in
+                                every way.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Value 2 -->
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100 text-center logoss hidden">
+                        <div class="card-body">
+                            <i class="fas fa-tools fa-4x mb-4 value-icon-2"></i>
+                            <h5 class="card-title">Quality Workmanship</h5>
+                            <p class="card-text">
+                                We take pride in our work and are committed to delivering the
+                                highest level of quality in all that we do. From installation
+                                to repair and maintenance, we strive for excellence in every
+                                aspect of our work.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Value 3 -->
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100 text-center logoss hidden">
+                        <div class="card-body">
+                            <i class="fas fa-shield-alt fa-4x mb-4 value-icon-2"></i>
+                            <h5 class="card-title">Integrity</h5>
+                            <p class="card-text">
+                                We believe that honesty and integrity are the foundation of
+                                any successful business relationship. We are committed to
+                                conducting ourselves with the highest level of professionalism
+                                and transparency in all that we do.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
+}
+</body>
+
+</html>
